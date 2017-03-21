fdb93656fe3ef79ec886e9668ae74ccebaec7b42
Luis A.Rivera Marquez <luis.rivera79@upr.edu>
2017-03-21 02:26:37 -0400

    Added a MineCoordinates class

---
+++ Minesweeper/src/MineCoordinates.java
@@ -0,0 +1,37 @@
+
+public class MineCoordinates {
+	
+	private double xPos;
+	private double yPos;
+	
+	
+	public MineCoordinates ( double x, double y){
+		
+		this.xPos= x;
+		this.yPos = y;
+		
+		}
+
+
+	public double getxPos() {
+		return xPos;
+	}
+
+
+	public void setxPos(double xPos) {
+		this.xPos = xPos;
+	}
+
+
+	public double getyPos() {
+		return yPos;
+	}
+
+
+	public void setyPos(double yPos) {
+		this.yPos = yPos;
+	}
+	
+	
+
+}
